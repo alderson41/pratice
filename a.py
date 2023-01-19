dd51b68ce27ff1e31a10398976c6c1a6b256f311
alderson41 <aldersonanirudh@gmail.com>
2023-01-19 14:52:17 +0530

    initial commit

---
+++ a.py
@@ -0,0 +1,15 @@
+class Queue:
+    def __init__(self, size=None) -> None:
+        self.size = size
+        self.__l = []
+        self.__l_size = 0
+
+    def enqueue(self, ele) -> None:
+        self.__l[-1] = ele
+        self.__l_size += 1
+
+    def dequeue(self) -> None:
+        del self.__l[0]
+        self.__l_size -= 1
+    
+print(isinstance(int, str))
